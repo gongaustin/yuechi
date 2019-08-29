e3181f43b4c7ceb76d7ee74d8f82c487911eb60a
GongJun <28402674@qq.com>
2019-08-29 17:51:40 +0800

    Swagger OK

--- src/main/java/com/gongjun/yuechi/controller/LoginController.java
+++ src/main/java/com/gongjun/yuechi/controller/LoginController.java
@@ -24,6 +24,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -34,7 +35,7 @@ import java.util.stream.Collectors;
  * @Author:GongJun
  * @Date:2019/8/29
  */
-@Api(value = "登录")
+@Api(value = "登录",description = "登录接口")
 @Controller
 public class LoginController {
     @Autowired
