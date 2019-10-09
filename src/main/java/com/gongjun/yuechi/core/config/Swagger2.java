acb4525c55f03f0a39ac7523c670d9217700ae91
GongJun <28402674@qq.com>
2019-10-09 09:36:31 +0800

    模块权限添加

--- src/main/java/com/gongjun/yuechi/core/config/Swagger2.java
+++ src/main/java/com/gongjun/yuechi/core/config/Swagger2.java
@@ -7,6 +7,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -30,7 +31,7 @@ public class Swagger2 {
                 //描述
                 .description("接口文档")
                 //创建人
-                //.contact(new Contact("LinXiuNan", "", ""))
+                .contact(new Contact("成都布林特信息技术有限公司", "www.sevencai.com", ""))
                 //版本号
                 .version("1.0").build();
     }
